62437c127ec23a41377a3d0b7ed1536dc9dff393
sujeet <sujeet7838@gmail.com>
2023-12-25 11:28:44 +0530

    category api modification

--- lib/features/category_page.dart
+++ lib/features/category_page.dart
@@ -19,10 +19,11 @@ class _CategoryPageState extends State<CategoryPage> {
         future: getCategoryData(),
         builder: (context, snapshort) {
           if (snapshort.hasData) {
-            return Container(
-              child: Column(
-                children: [
-                  ListView.builder(
+            return Column(
+              children: [
+                Container(
+                  height: 300,
+                  child: ListView.builder(
                       padding: const EdgeInsets.symmetric(
                           horizontal: 10, vertical: 10),
                       itemCount: snapshort.data!.data.mainCourse.length,
@@ -36,18 +37,36 @@ class _CategoryPageState extends State<CategoryPage> {
                           padding: const EdgeInsets.symmetric(
                               horizontal: 10, vertical: 10),
                           child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
                             mainAxisAlignment: MainAxisAlignment.start,
                             children: [
-                              Text(snapshort
-                                  .data!.data.mainCourse[index].productName
-                                  .toString()),
-                              Text(snapshort
-                                  .data!.data.mainCourse[index].categoryName
-                                  .toString()),
-                              Text(snapshort
-                                  .data!.data.mainCourse[index].productPrice
-                                  .toString()),
+                              Row(
+                                children: [
+                                  Image(
+                                    image: NetworkImage(
+                                      'https://click4technologies.com/design/t20/bollywood-restarant/${snapshort.data!.data.mainCourse[index].productImage}',
+                                    ),
+                                    fit: BoxFit.fill,
+                                    height: 100.0,
+                                  ),
+                                  Column(
+                                    children: [
+                                      Text(snapshort.data!.data
+                                          .mainCourse[index].productName
+                                          .toString()),
+                                      Text(snapshort.data!.data
+                                          .mainCourse[index].categoryName
+                                          .toString()),
+                                      Text(snapshort.data!.data
+                                          .mainCourse[index].productPrice
+                                          .toString()),
+                                    ],
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
                               Container(
                                   height: 50,
                                   width: double.infinity,
@@ -82,8 +101,94 @@ class _CategoryPageState extends State<CategoryPage> {
                           ),
                         );
                       }),
-                ],
-              ),
+                ),
+
+                //stater
+                Container(
+                  height: 300,
+                  child: ListView.builder(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      itemCount: snapshort.data!.data.starter.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(10)),
+                          margin: const EdgeInsets.only(bottom: 10),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Image(
+                                    image: NetworkImage(
+                                      'https://click4technologies.com/design/t20/bollywood-restarant/${snapshort.data!.data.mainCourse[index].productImage}',
+                                    ),
+                                    fit: BoxFit.fill,
+                                    height: 100.0,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      children: [
+                                        Text(snapshort.data!.data.starter[index]
+                                            .productName
+                                            .toString()),
+                                        Text(snapshort.data!.data.starter[index]
+                                            .categoryName
+                                            .toString()),
+                                        Text(snapshort.data!.data.starter[index]
+                                            .productPrice
+                                            .toString()),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Container(
+                                  height: 50,
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      borderRadius: BorderRadius.circular(10)),
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 10),
+                                  child: DropdownButton(
+                                      value: newData,
+                                      isDense: true,
+                                      underline: Container(),
+                                      hint: const Text('Select value'),
+                                      isExpanded: true,
+                                      icon:
+                                          const Icon(Icons.keyboard_arrow_down),
+                                      items: snapshort.data!.data
+                                          .mainCourse[index].productVariation
+                                          .map((item) {
+                                        return DropdownMenuItem(
+                                          value:
+                                              item.variationValueId.toString(),
+                                          child: Text(item.variationValueName
+                                              .toString()),
+                                        );
+                                      }).toList(),
+                                      onChanged: (value) {
+                                        newData = value;
+                                        setState(() {});
+                                      }))
+                            ],
+                          ),
+                        );
+                      }),
+                ),
+              ],
             );
           } else {
             return const Center(
